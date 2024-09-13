1bedc08f238296ff02358f102899dcb3258b0bd7
zoldikk <167327016+zoldikk@users.noreply.github.com>
2024-09-13 16:23:54 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,59 +1,49 @@
-import fetch from 'node-fetch';
+const axios = require('axios');
 
-// Function to shorten URLs
 async function shortenUrl(longUrl) {
     const apiUrl = 'https://cleanuri.com/api/v1/shorten';
-    const response = await fetch(apiUrl, {
-        method: 'POST',
-        body: new URLSearchParams({ url: longUrl }),
-    });
-    if (response.ok) {
-        const data = await response.json();
-        return data.result_url;
-    } else {
+    try {
+        const response = await axios.post(apiUrl, new URLSearchParams({ url: longUrl }));
+        return response.data.result_url;
+    } catch (error) {
+        console.error('Error shortening URL:', error);
         return null;
     }
 }
 
-// Function to decode hex string
 function dec(hexString) {
-    let str = '';
-    for (let i = 0; i < hexString.length; i += 2) {
-        str += String.fromCharCode(parseInt(hexString.substr(i, 2), 16));
-    }
-    return str;
+    return Buffer.from(hexString, 'hex').toString('latin1');
 }
 
-// API handler
-export default async function handler(req, res) {
-    const { lop } = req.query;
-
-    if (!lop) {
-        return res.status(400).json({ error: "Parameter 'lop' is required" });
-    }
-
+async function info(lop) {
     const phones = ["iPhone", "Samsung", "redmi", "OnePlus", "Sony", "Huawei"];
     const phone = phones[Math.floor(Math.random() * phones.length)];
     const longText = dec(lop);
     const id = "في فريق";
 
-    let responseText = '';
-
     if (longText.includes('google')) {
-        const startLink = longText.indexOf('https');
-        const endLink = longText.indexOf('', startLink);
-        const link = longText.substring(startLink, endLink);
+        const link = extractLink(longText);
         const shortUrl = await shortenUrl(link);
-        responseText = `[b][c]~ المحاكمة:\n[00ffff]حالة لاعب: ${id}\nهاتف لاعب: ${phone}\nربط اساسي: Google\nصورة لاعب: [00ff00]${shortUrl}`;
+        const nor = "Google";
+        console.log(`[b][c]~ المحاكمة:\n[00ffff]حالة لاعب: ${id}\nهاتف لاعب: ${phone}\nربط اساسي: ${nor}\nصورة لاعب: [00ff00]${shortUrl}`);
     }
 
     if (longText.includes('facebook')) {
-        const startLink = longText.indexOf('https');
-        const endLink = longText.indexOf('', startLink);
-        const link = longText.substring(startLink, endLink);
+        const link = extractLink(longText);
         const shortUrl = await shortenUrl(link);
-        responseText = `[b][c]~ المحاكمة:\n[00ffff]حالة لاعب: ${id}\nهاتف لاعب: ${phone}\nربط اساسي: Facebook\nصورة لاعب: [00ff00]${shortUrl}`;
+        const nor = "Facebook";
+        console.log(`[b][c]~ المحاكمة:\n[00ffff]حالة لاعب: ${id}\nهاتف لاعب: ${phone}\nربط اساسي: ${nor}\nصورة لاعب: [00ff00]${shortUrl}`);
     }
+}
 
-    res.status(200).send(responseText);
-            }
+function extractLink(text) {
+    const ap = 'https';
+    const dp = '';
+    const startLink = text.indexOf(ap);
+    const endLink = text.indexOf(dp, startLink);
+    return text.substring(startLink, endLink);
+}
+
+// Replace 'lop' with your actual hex string input
+const lop = 'your_hex_string_here';
+info(lop);
